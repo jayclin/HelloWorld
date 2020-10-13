1a45c94cf0ddd1c59197c5d618f3e121795d91aa
jayclin <chenglin0656@163.com>
2020-10-13 08:11:32 +0800

    jayclin

---
+++ app/src/main/java/ListViewActivity.java
@@ -0,0 +1,19 @@
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.helloworld.R;
+
+/**
+ * @author： chenglin
+ * @date： 2020-09-24 21:12
+ * @description：
+ */
+
+public class ListViewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
